419c107209898ca554161678f6bf8e8cc91d06c1
admin <admin@DESKTOP-7U877N6>
2020-06-18 13:34:32 +0530

    firstcommit

---
+++ src/testPac/SimpleJavProg.java
@@ -0,0 +1,12 @@
+package testPac;
+
+public class SimpleJavProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 6;
+		System.out.println("print value of a: "+a);
+		System.out.println(" Simple isn't it:)\n Welcome to JAVA");
+	}
+
+}
